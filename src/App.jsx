c1df86db1cd607c9431597ebc5c7a64b4f9b69d1
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-04-10 20:03:49 +0500

    implemented pricing component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
 import Services from "./components/Services";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Benefits />
         <Collaboration />
         <Services />
+        <Pricing />
       </div>
 
       <ButtonGradient />
